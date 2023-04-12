bd67731ca4cf52241bfa928c15921cd93a64a320
derilmnzes <derilmenezes02@gmail.com>
2023-04-12 11:37:47 +0400

    new changes

--- src/Middlewear/multer.js
+++ src/Middlewear/multer.js
@@ -1,13 +1,21 @@
+
+const path = require('path');
+
+// Joining __dirname with parent directory
+const joinedPath = path.join(__dirname, '..','..','public/uploads/images');
+console.log(joinedPath);
+// Output: '/path/to/parent/directory' (assuming __dirname is '/path/to/current/directory')
+
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/images');
+    cb(null, joinedPath);
   },
   filename: function (req, file, cb) {
-    console.log(file,'file')
+    
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + '-' + uuidv4();
     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
   }
